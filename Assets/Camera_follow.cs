56aeed00580e7f31501cd21bfaab4691305921cb
rootfiz <31610884+rootfiz@users.noreply.github.com>
2021-01-16 10:58:10 +0600

    Initial commit

---
+++ Assets/Camera_follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+
+
+        if(Camera.main.fieldOfView <= 130)
+        {
+            Camera.main.fieldOfView = Camera.main.fieldOfView + (10f * Time.deltaTime);
+        }
+        
+        
+    }
+}
---
+++ Assets/Rocket.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody2D rocket;
+    [SerializeField]
+    float thrust;
+    [SerializeField]
+    float turn_thrust;
+    void Start()
+    {
+        rocket = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.A)) //left
+        {
+            AddForceAt_left(thrust, turn_thrust);
+        } 
+        
+        
+        if (Input.GetKey(KeyCode.D)) //right
+        {
+            AddForceAt_right(thrust,turn_thrust);
+        }
+
+
+    }
+
+    public void AddForceAt_right(float force, float angle)
+    {
+        float xcomponent = Mathf.Cos(angle * Mathf.PI / 180) * force;
+        float ycomponent = Mathf.Sin(angle * Mathf.PI / 180) * force;
+        Vector3 right_force = new Vector3(ycomponent, xcomponent, 0);
+        rocket.AddForce(right_force);
+    }
+
+    public void AddForceAt_left(float force, float angle)
+    {
+        float xcomponent = Mathf.Cos(angle * Mathf.PI / 180) * force;
+        float ycomponent = Mathf.Sin(angle * Mathf.PI / 180) * force;
+        Vector3 left_force = new Vector3(-ycomponent, xcomponent, 0);
+        rocket.AddForce(left_force);
+    }
+
+}
